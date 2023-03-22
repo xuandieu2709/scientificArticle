e3095e78ecf11f6973eb6017835621d3de2f9d41
xuandieu2709 <xuandieu12300@gmail.com>
2023-03-22 22:28:02 +0700

    update admin and post

--- js/main.js
+++ js/main.js
@@ -21,9 +21,44 @@ function topFunction() {
 $(document).ready(function () {
   $("div#div-post-wrap").slideUp(2000).slideDown(2000);
 });
-$(document).on('click','button#showTab', function () {
+$(document).on('click', 'button#showTab', function () {
   $('div#menu').css('display', 'block');
 });
-$(document).on('click','span#close-menu', function () {
+$(document).on('click', 'span#close-menu', function () {
   $('div#menu').css('display', 'none');
+});
+
+$(document).on('change', 'select#selectNumberAuthor', function () {
+  var value = $(this).find(":selected").val();
+  var text = '<div class="col-4 mb-2">'
+    + '<label for="">Tên tác giả <span style="color: red;">*</span></label>'
+    + '</div>'
+    +'<div class="col-8 mb-2">'
+    + '<input type="text" required class="input-group">'
+    + '</div>'
+    + '<div class="col-4 mb-2">'
+    + '<label for="">Đơn vị <span style="color: red;">*</span></label>'
+    + '</div>'
+    + '<div class="col-8 mb-2">'
+    + '<input type="text" class="input-group">'
+    + '</div>'
+    + '<div class="col-4 mb-2">'
+    + '<label for="">Email liên hệ <span style="color: red;">*</span></label>'
+    + '</div>'
+    + '<div class="col-8 mb-2">'
+    + '<input type="text" class="input-group">'
+    + '</div>'
+    + '<div class="col-4 mb-2">'
+    + '<label for="">Số điện thoại <span style="color: red;">*</span></label>'
+    + '</div>'
+    + '<div class="col-8 mb-2">'
+    + '<input type="text" class="input-group">'
+    + '</div>'
+    +'<hr style="background-color: #264653; width: 100%;height: 1px; margin: 1em;">';
+    $("div#divAuthor").empty()
+    for(i = 0;i<Number(value);i++)
+    {
+      $("div#divAuthor").prepend(text);
+    }
+
 });
